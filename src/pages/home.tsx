c83b2983d9fc0145aca19b9e354c2800aab87fbb
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-01-04 18:11:46 +0000

    Finish home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,13 +2,16 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '@styles/Home.module.scss'
+import styles from '@styles/pages/Home.module.scss'
 import React, { useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../store/hooks'
 import Button from '@components/Button/Button'
 import FloatingButton from '@components/Button/FloatingButton'
-import SendIcon from '@assets/icons/send.svg'
+import TwitterIcon from '@assets/icons/twitter.svg'
+import LinkedinIcon from '@assets/icons/linkedin.svg'
+import GitHub from '@assets/icons/github.svg'
 import { getBaseInfo, PersonalDataSelector } from '@store/index'
+import { ContactData } from '@shared/types'
 
 const Home: NextPage = () => {
   const dispatch = useAppDispatch()
@@ -18,19 +21,46 @@ const Home: NextPage = () => {
     dispatch(getBaseInfo())
   }, [])
 
+  let getContactsIcons = (contacts: ContactData) => {
+    {
+      Object.keys(data.contacts).map((key, index) => (
+        <a href={data.contacts[key]}></a>
+      ))
+    }
+  }
+
   return (
-    <div className="container container-full-page">
-      Home page <span>Teste</span>
-      <p>{data?.description}</p>
-      <h1>Teste 1</h1>
-      <Button onClick={() => {}}>
-        <span>Send</span>
-        <SendIcon />
-      </Button>
-      <Button onClick={() => {}} type="primary">
-        <span>Send</span>
-        <SendIcon />
-      </Button>
+    <div
+      className={['container container-full-page', styles['home-page']].join(
+        ' '
+      )}
+    >
+      {pending ? (
+        'loading'
+      ) : (
+        <>
+          <div className="typhography typhography-title">{data.title}</div>
+          <div className="typhography typhography-subtitle">
+            {data.subtitle}
+          </div>
+          <br />
+          <div>{data.description}</div>
+          <br />
+          <div className={styles.contacts}>
+            {Object.keys(data.contacts).map((key, index) => (
+              <a href={data.contacts[key]} target="_blank">
+                {key === 'linkedin' ? (
+                  <LinkedinIcon />
+                ) : key === 'twitter' ? (
+                  <TwitterIcon />
+                ) : (
+                  <GitHub />
+                )}
+              </a>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/shared/types.tsx
+++ src/shared/types.tsx
@@ -1,23 +1,26 @@
 // ============================== TYPES ==============================
 
 export type ActionState<T> = {
-  data?: T
+  data: T
   pending: boolean
   error: boolean
 }
+
+export type ContactData = { [key: string]: string }
 
 export type PersonalData = {
   title: string
   subtitle: string
   description: string
+  contacts: ContactData
 }
 
 // ============================== FUNCTIONS ==============================
 
 export function CreateActionState<T>(arg?: T): ActionState<T> {
   const type: ActionState<T> = {
-    data: arg,
-    pending: false,
+    data: arg || ({} as T),
+    pending: true,
     error: false
   }
 
