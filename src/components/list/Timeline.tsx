f423480d2ae1e99617723b030d8ea34b299ce68c
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-01-16 20:48:18 +0000

    Redo timeline to center content and have a line to the entire container width

--- src/components/list/Timeline.tsx
+++ src/components/list/Timeline.tsx
@@ -12,30 +12,33 @@ type Props = {
 export default function Timeline({ data }: Props) {
   return (
     <div className={styles.timeline}>
-      {data.map((item, index) => {
-        return (
-          <div key={index} className={styles.bubble}>
-            <div className={styles.box}>
-              <div className={styles.main}>
-                <div className="typhography typhography-sub-title typhography--primary typhography--strong">
-                  <Link href={`education/${item.id}`}>
-                    <a>{item.title}</a>
-                  </Link>
+      <div className={styles.container}>
+        {data.map((item, index) => {
+          return (
+            <div key={index} className={styles.bubble}>
+              <div className={styles.box}>
+                <div className={styles.main}>
+                  <div className="typhography typhography-sub-title typhography--primary typhography--strong">
+                    <Link href={`education/${item.id}`}>
+                      <a>{item.title}</a>
+                    </Link>
+                  </div>
+                  <div className="typhography typhography-sub-sub-title">
+                    {item.entity}
+                  </div>
                 </div>
-                <div className="typhography typhography-sub-sub-title">
-                  {item.entity}
-                </div>
+                <span className="typhography typhography-sub-sub-title">
+                  {item.startDate}
+                  {item.endDate && ` - ${item.endDate}`}
+                </span>
+                <div className={styles.triangle}></div>
               </div>
-              <span className="typhography typhography-sub-sub-title">
-                {item.startDate}
-                {item.endDate && ` - ${item.endDate}`}
-              </span>
-              <div className={styles.triangle}></div>
+              <div className={styles.dot}></div>
             </div>
-            <div className={styles.dot}></div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
+      <div className={styles.line}></div>
     </div>
   )
 }
