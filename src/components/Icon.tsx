15782bf95ee4947a75c5926163f7a95446603008
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-02-04 16:07:58 +0000

    Add new icons used in skills and add skills tab element

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import ArrowBackIcon from '@assets/icons/arrowBack.svg'
 import ArrowForwardIcon from '@assets/icons/arrowforward.svg'
-import ArrowDownIcon from '@assets/icons/arrowDown.svg'
 import ExternalLinkIcon from '@assets/icons/externalLink.svg'
 import BulbIcon from '@assets/icons/bulb.svg'
 import TwitterIcon from '@assets/icons/twitter.svg'
 import LinkedinIcon from '@assets/icons/linkedin.svg'
-import GitHub from '@assets/icons/github.svg'
+import GitHubIcon from '@assets/icons/github.svg'
+import CSharpIcon from '@assets/icons/csharp.svg'
+import CLanguageIcon from '@assets/icons/c.svg'
+import CSSIcon from '@assets/icons/css.svg'
+import FlutterIcon from '@assets/icons/flutter.svg'
+import HTMLICon from '@assets/icons/html.svg'
+import JavaIcon from '@assets/icons/java.svg'
+import JavascriptIcon from '@assets/icons/javascript.svg'
+import NodeJSIcon from '@assets/icons/nodejs.svg'
+import PythonIcon from '@assets/icons/python.svg'
+import ReactIcon from '@assets/icons/react.svg'
+import SendIcon from '@assets/icons/send.svg'
+import SqlIcon from '@assets/icons/sql.svg'
 
 type Props = {
   name: string
@@ -19,12 +30,23 @@ export default function Icon({ name }: Props) {
         {
           arrowBack: <ArrowBackIcon />,
           arrowForward: <ArrowForwardIcon />,
-          arrowDown: <ArrowDownIcon />,
           externalLink: <ExternalLinkIcon />,
           bulb: <BulbIcon />,
           twitter: <TwitterIcon />,
           linkedin: <LinkedinIcon />,
-          github: <GitHub />
+          github: <GitHubIcon />,
+          csharp: <CSharpIcon />,
+          clanguage: <CLanguageIcon />,
+          css: <CSSIcon />,
+          flutter: <FlutterIcon />,
+          html: <HTMLICon />,
+          java: <JavaIcon />,
+          javascript: <JavascriptIcon />,
+          nodejs: <NodeJSIcon />,
+          python: <PythonIcon />,
+          react: <ReactIcon />,
+          send: <SendIcon />,
+          sql: <SqlIcon />
         }[name]
       }
     </>
--- src/containers/Skills.tsx
+++ src/containers/Skills.tsx
@@ -3,10 +3,14 @@ import { SkillModel } from '@shared/types'
 import styles from '@styles/containers/Skills.module.scss'
 import Typhography from '@components/Typhography'
 import Container from '@components/Container'
+import { useState } from 'react'
+import Icon from '@components/Icon'
 
 export default function Skills() {
-  const data = useData<SkillModel>('skills.json')
-  console.log(data)
+  const [activeTabIndex, setActiveTabIndex] = useState(0)
+  const data = useData<SkillModel[]>('skills.json')
+  const navItems = data.map(item => item.type)
+
   return (
     <Container sectionId="skills" className={styles['skills-container']} page>
       <Typhography type="title" color="primary" strong upperCase header>
@@ -17,6 +21,62 @@ export default function Skills() {
         alt="Skills cover image"
         className={styles['cover-image']}
       />
+      <div className={styles.tab}>
+        <ul className={styles.nav}>
+          {navItems.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={[
+                  styles['nav-item'],
+                  activeTabIndex === index && styles['nav-item--active']
+                ].join(' ')}
+                onClick={() => setActiveTabIndex(index)}
+              >
+                <Typhography type="sub-title" strong upperCase color="primary">
+                  {item}
+                </Typhography>
+              </li>
+            )
+          })}
+        </ul>
+        <div className={styles.content}>
+          {data.map((skillType, index) => {
+            return (
+              <div
+                className={styles['tab-pane']}
+                key={index}
+                style={{ transform: `translateX(-${activeTabIndex * 100}%)` }}
+              >
+                {skillType.section.map((section, index) => {
+                  return (
+                    <div key={index} className={styles['pane-section']}>
+                      <Typhography
+                        type="sub-sub-title"
+                        strong
+                        upperCase
+                        color="primary"
+                      >
+                        {section.title}
+                      </Typhography>
+                      <div className={styles['skills-content']}>
+                        {section.skills.map((skill, index) => {
+                          return (
+                            <div key={index} className={styles.skill}>
+                              {skill.iconName && <Icon name={skill.iconName} />}
+                              {skill.name}
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </Container>
   )
 }
