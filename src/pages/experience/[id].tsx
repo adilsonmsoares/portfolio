3ffb1fbf071ee21ab3a30e000224b15e82a702e0
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-02-11 22:33:30 +0000

    Add experience detail page

---
+++ src/pages/experience/[id].tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import { InferGetServerSidePropsType, GetServerSidePropsContext } from 'next'
+import useData from '@hooks/useData'
+import { DetailViewModel } from '@shared/types'
+import DetailViewer from '@components/DetailViewer'
+
+function ExperienceDetails({
+  data
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 0) // wait to setup load
+  }, [])
+
+  return <DetailViewer isLoading={isLoading} data={data} />
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  let { id } = context.query
+  let list = useData<DetailViewModel[]>('experience.json')
+  let data = list.find(e => e.id === id)
+
+  return {
+    props: { data }
+  }
+}
+
+export default ExperienceDetails
