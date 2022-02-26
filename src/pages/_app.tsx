7fcdff1ae1d13626096d39f904639419416f8e9c
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-02-26 00:07:30 +0000

    Add NextNProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
-import useDarkMode from 'use-dark-mode'
+import NextNProgress from 'nextjs-progressbar'
 import '@styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const darkmode = useDarkMode(true)
-
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
+      <NextNProgress
+        color="var(--special-color)"
+        options={{ showSpinner: false }}
+      />
       <Component {...pageProps} />
     </>
   )
