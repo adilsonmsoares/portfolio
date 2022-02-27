ec1ac8955466d5753ff9e656c26c0523bec042bd
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-02-27 23:57:02 +0000

    Remove webpack alias and repath tsconfig paths

--- next.config.js
+++ next.config.js
@@ -12,14 +12,6 @@ module.exports = {
       use: ["@svgr/webpack"]
     });
 
-    config.resolve.alias["@styles"] = path.resolve(__dirname, "styles/");
-    config.resolve.alias["@components"] = path.resolve(__dirname, "components/");
-    config.resolve.alias["@containers"] = path.resolve(__dirname, "containers/");
-    config.resolve.alias["@assets"] = path.resolve(__dirname, "public/");
-    config.resolve.alias["@repositories"] = path.resolve(__dirname, "repositories/");
-    config.resolve.alias["@shared"] = path.resolve(__dirname, "shared/");
-    config.resolve.alias["@hooks"] = path.resolve(__dirname, "hooks/");
-
     return config;
   }
 }
