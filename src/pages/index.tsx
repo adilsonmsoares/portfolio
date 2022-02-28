482f59c01aa706d6d475da41e20c54834fb5079e
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-02-28 23:31:16 +0000

    Readd containers import and change webpack alias

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import Layout from '@components/Layout'
+import About from '@containers/About'
+import Contact from '@containers/Contact'
+import Education from '@containers/Education'
+import Experience from '@containers/Experience'
+import HomeContainer from '@containers/Home'
+import Project from '@containers/Project'
+import Skills from '@containers/Skills'
 import type { NextPage } from 'next'
 
 const Index: NextPage = () => {
-  return <Layout scrollSmooth>Teste deploy</Layout>
+  return (
+    <Layout scrollSmooth>
+      <HomeContainer />
+      <About />
+      <Education />
+      <Skills />
+      <Experience />
+      <Project />
+      <Contact />
+    </Layout>
+  )
 }
 
 export default Index
