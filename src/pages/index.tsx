d318d6ccd5490e067dd26653d344c567e191137b
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-02-28 22:27:29 +0000

    Import without alias

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Layout from '@components/Layout'
-import About from '@containers/About'
-import Contact from '@containers/Contact'
-import Education from '@containers/Education'
-import Experience from '@containers/Experience'
-import HomeContainer from '@containers/Home'
-import Project from '@containers/Project'
-import Skills from '@containers/Skills'
 import type { NextPage } from 'next'
+import About from '../containers/About'
+import Contact from '../containers/Contact'
+import Education from '../containers/Education'
+import Experience from '../containers/Experience'
+import HomeContainer from '../containers/Home'
+import Project from '../containers/Project'
+import Skills from '../containers/Skills'
 
 const Index: NextPage = () => {
   return (
