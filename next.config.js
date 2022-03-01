c0d9a7457d23b7719bc7f243d93dc20661fd683b
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-03-01 23:53:56 +0000

    Fix basePath removing slash

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'productions'
 
 module.exports = {
   reactStrictMode: true,
-  basePath: isProd ? '/porfolio/' : '',
-  assetPrefix: isProd ? '/porfolio/' : '',
+  basePath: isProd ? '/porfolio' : '',
+  assetPrefix: isProd ? '/porfolio' : '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
