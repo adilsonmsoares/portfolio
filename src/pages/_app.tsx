23efc6352fdc0995f9494b5c65d0e0b7d68c98dc
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-03-02 23:35:50 +0000

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || ''
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href={prefix + 'favicon.svg'} />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
       <NextNProgress
