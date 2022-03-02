37063d987803b7df4919809f078046716d4e2331
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-03-02 18:21:35 +0000

    Get request pages ID from  window instead of next router
    When a refresh is made the router will not have any id parameter so to pages works after refresh is better to get the id from window location

--- src/pages/education/[id].tsx
+++ src/pages/education/[id].tsx
@@ -1,15 +1,13 @@
 import DetailViewer from '@components/DetailViewer'
 import useData from '@hooks/useData'
 import { DetailViewModel } from '@shared/types'
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
 export default function EducationDetails() {
   const [data, setdata] = useState<DetailViewModel>()
-  const router = useRouter()
 
   useEffect(() => {
-    let { id } = router.query
+    let id = window.location.pathname.split('/').pop()
     let list = useData<DetailViewModel[]>('education.json')
     let data = list.find(e => e.id === id)
     setdata(data)
--- src/pages/project/[id].tsx
+++ src/pages/project/[id].tsx
@@ -1,15 +1,13 @@
 import DetailViewer from '@components/DetailViewer'
 import useData from '@hooks/useData'
 import { DetailViewModel } from '@shared/types'
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
 export default function ExperienceDetails() {
   const [data, setdata] = useState<DetailViewModel>()
-  const router = useRouter()
 
   useEffect(() => {
-    let { id } = router.query
+    let id = window.location.pathname.split('/').pop()
     let list = useData<DetailViewModel[]>('projects.json')
     let data = list.find(e => e.id === id)
     setdata(data)
