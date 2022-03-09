bb2fc620d180b325eb949be3077deef37a0f77bc
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-03-09 23:51:47 +0000

    Use base url in meta tags image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ import Document, {
   NextScript
 } from 'next/document'
 
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || ''
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ''
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -27,26 +27,17 @@ export default class MyDocument extends Document {
 
           {/* Open Graph / Facebook */}
           <meta property="og:type" content="website" />
-          <meta
-            property="og:url"
-            content="https://adilsonmsoares.github.io/portfolio/"
-          />
+          <meta property="og:url" content={baseUrl} />
           <meta property="og:title" content="Adilson Soares" />
           <meta
             property="og:description"
             content="Adilson Soares is a Software Developer and Engineer trained to build well-structured applications in terms of architecture and code quality in order to promote a good user experience."
           />
-          <meta
-            property="og:image"
-            content={'%PUBLIC_URL%/images/metaTag.jpg'}
-          />
+          <meta property="og:image" content={baseUrl + '/images/metaTag.jpg'} />
 
           {/* Twitter */}
           <meta property="twitter:card" content="summary_large_image" />
-          <meta
-            property="twitter:url"
-            content="https://adilsonmsoares.github.io/portfolio/"
-          />
+          <meta property="twitter:url" content={baseUrl} />
           <meta property="twitter:title" content="Adilson Soares" />
           <meta
             property="twitter:description"
@@ -54,7 +45,7 @@ export default class MyDocument extends Document {
           />
           <meta
             property="twitter:image"
-            content={'%PUBLIC_URL%/portfolio/images/metaTag.jpg'}
+            content={baseUrl + '/images/metaTag.jpg'}
           />
           <meta charSet="utf-8" />
           <link
