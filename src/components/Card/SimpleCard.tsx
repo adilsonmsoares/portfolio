79eacb4770e1f0be361e4027dc6dd26886ef80a5
adilsonsoares1996 <39263773+adilsonmsoares@users.noreply.github.com>
2022-03-31 11:22:24 +0100

    Increased cards link area

--- src/components/Card/SimpleCard.tsx
+++ src/components/Card/SimpleCard.tsx
@@ -13,9 +13,9 @@ const prefix = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 export default function SimpleCard({ data, page }: Props) {
   return (
-    <div className={styles['card']}>
-      <Link href={`/${page}/${data.id}`}>
-        <a className={styles['main-info']}>
+    <Link href={`/${page}/${data.id}`}>
+      <a className={styles['card']}>
+        <div className={styles['main-info']}>
           <div className={styles.header}>
             <Typhography type="sub-title" color="special" strong>
               {data.title}
@@ -30,18 +30,18 @@ export default function SimpleCard({ data, page }: Props) {
           <div className={styles.description}>
             {data.sectionDescription[0].description[0]}
           </div>
-        </a>
-      </Link>
-      <div className={styles['entity-logo']}>
-        {data.entity.logoUrl ? (
-          <img
-            src={prefix + data.entity.logoUrl}
-            alt={data.entity.name + ' Logo'}
-          />
-        ) : (
-          <span>No Logo Available</span>
-        )}
-      </div>
-    </div>
+        </div>
+        <div className={styles['entity-logo']}>
+          {data.entity.logoUrl ? (
+            <img
+              src={prefix + data.entity.logoUrl}
+              alt={data.entity.name + ' Logo'}
+            />
+          ) : (
+            <span>No Logo Available</span>
+          )}
+        </div>
+      </a>
+    </Link>
   )
 }
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -10,38 +10,39 @@ type Props = {
 }
 
 export default function Timeline({ data, page }: Props) {
-  const renderMainInfo = (item: DetailViewModel) => {
+  const renderBoxContent = (item: DetailViewModel) => {
     return (
       <>
-        <div className={styles.title}>
-          <Typhography type="sub-title" color="special" strong>
-            {item.title}
+        <div className={styles.main}>
+          <div className={styles.title}>
+            <Typhography type="sub-title" color="special" strong>
+              {item.title}
+            </Typhography>
+            <Typhography type="sub-sub-title">{item.entity.name}</Typhography>
+          </div>
+          <Typhography type="sub-sub-title">
+            {item.startDate}
+            {item.endDate && ` - ${item.endDate}`}
           </Typhography>
-          <Typhography type="sub-sub-title">{item.entity.name}</Typhography>
         </div>
-        <Typhography type="sub-sub-title">
-          {item.startDate}
-          {item.endDate && ` - ${item.endDate}`}
-        </Typhography>
+        <div className={styles.triangle}></div>
       </>
     )
   }
+
   return (
     <div className={styles.timeline}>
       <div className={styles.container}>
         {data.map((item, index) => {
           return (
             <div key={index} className={styles.bubble}>
-              <div className={styles.box}>
-                {page ? (
-                  <Link href={`/${page}/${item.id}`}>
-                    <a className={styles.main}>{renderMainInfo(item)}</a>
-                  </Link>
-                ) : (
-                  <div className={styles.main}>{renderMainInfo(item)}</div>
-                )}
-                <div className={styles.triangle}></div>
-              </div>
+              {page ? (
+                <Link href={`/${page}/${item.id}`}>
+                  <a className={styles.box}>{renderBoxContent(item)}</a>
+                </Link>
+              ) : (
+                <div className={styles.box}>{renderBoxContent(item)}</div>
+              )}
               <div className={styles.dot}></div>
             </div>
           )
